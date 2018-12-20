daf7967f5de602849535d34fcf787b1103f407be
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-12-19 18:32:26 -0800

    add stock array of friend objects

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+// Assign a variable to hold the array of friend objects
+// Survey scores are randomly assigned until the app is up and running, then my "friends" can fill them out themselves
+var friendsArray = [
+	{
+		name: "Christie Byrne",
+		photo: "https://media.licdn.com/dms/image/C5603AQECPixmr7jqFw/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=LDEvJAEg7SRgnKf1mou9IkXY98z9x-yYCHY5sXA_nes",
+		scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 2]
+    },
+    {
+		name: "Lyle Farrell",
+		photo: "https://media.licdn.com/dms/image/C5103AQFZi4GyU7wN3Q/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=a2OqzJ3Q4Ymqlm-ivua7c20v-FK07kNchkmSHLQb1eQ",
+		scores: [2, 4, 1, 5, 3, 3, 2, 1, 4, 5]
+	},
+	{
+		name: "Isabel Arcones",
+		photo: "https://media.licdn.com/dms/image/C4E03AQHw_hRHeMNWgw/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=wL0GAIQj6NM4oi9UUgjOLkR8Cqa_wosrLNhFjLhkwwU",
+		scores: [3, 3, 5, 1, 2, 3, 1, 2, 2, 5]
+    },
+    {
+		name: "Saranya Mohandas",
+		photo: "https://media.licdn.com/dms/image/C5103AQFKbQ0Z1HwWtw/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=tpmxXaaBH0_uESBQT2iGxfhTHj2qnQKQpZR5c0tzFJs",
+		scores: [2, 1, 5, 3, 2, 2, 2, 5, 1, 3]
+    },
+    {
+		name: "Michael True",
+		photo: "https://media.licdn.com/dms/image/C5603AQHKe3FGS7Dq3A/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=rY_AwFm8VEzZBCOQzlvhdIPKoHtD7_cLvm9N7VigEuU",
+		scores: [3, 4, 1, 5, 1, 3, 5, 2, 2, 1]
+    },
+    {
+		name: "Paul Rogers",
+		photo: "https://media.licdn.com/dms/image/C4D03AQEA28_0bhlehA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=87i3FCP6D1PcKYccFH38dfSxXkOU_eLqF78U3Qevo1g",
+		scores: [2, 2, 1, 5, 3, 4, 3, 2, 1, 5]
+    },
+    {
+		name: "Sasha Patsel",
+		photo: "https://media.licdn.com/dms/image/C4E03AQFdezVeoePcmA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=kO89TLU6cZo4VrH580vssPwtD9Df1afkBBL-cWsrf2U",
+		scores: [3, 4, 1, 5, 2, 3, 2, 1, 4, 1]
+    },
+    {
+		name: "Ryan Moore",
+		photo: "https://media.licdn.com/dms/image/C5603AQHk641g4iAxSw/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=Ffrz7UYvgdvdppTRjWbZKI9S8SUx28uAhH2P1YqDht4",
+		scores: [5, 5, 1, 4, 1, 3, 1, 4, 5, 2]
+    },
+    {
+		name: "Jacob Moore",
+		photo: "https://media.licdn.com/dms/image/C5603AQGu8t3jTV_ckQ/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=da99OjYAHSsrdiYyg4Wzq72g92BfWHoOrAhT-lwis50",
+		scores: [3, 2, 4, 1, 5, 2, 1, 4, 5, 4]
+    },
+    {
+		name: "Melika Kalbasi",
+		photo: "https://media.licdn.com/dms/image/C5603AQGym6-j4udCDA/profile-displayphoto-shrink_800_800/0?e=1550707200&v=beta&t=5Jks5SvHurO9EgvLLn_vVU_sIECRWwZbt2ULkIwPTnQ",
+		scores: [5, 1, 1, 3, 2, 5, 4, 4, 2, 3]
+	}
+];
+// Export the friendsArray
+module.exports = friendsArray;
