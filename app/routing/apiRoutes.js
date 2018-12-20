7310908b90f26627530f84bd7de136df120f50fa
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-12-19 17:55:19 -0800

    fill out pseudocode for compatibility logic with actual code in apiRoutes; import apiRoutes and htmlRoutes to server file; Create an instance of Express to listen on the designated PORT in server file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -29,7 +29,30 @@ module.exports = function (app) {
         var friendMatch;
         // Instantiate a variable with the maximum possible difference allowed by 10 survey questions + 1 (50 + 1)
         var maximumDifference = 51; // This can be changed to totalDifference since they'll be doing the same thing, per Step 4
-        // Iterate through each element (object) in the friendData array
+        // Iterate through each element (object) in the friendData array - assign parameter 'friend' as placeholder for each object
+        friendData.forEach(function (friend) {
+            // Instiatiate a variable to hold the sums of each difference calculation
+            var differenceSum = 0;
+            // Instantiate a variable to hold the 'scores' of each friend's survey results
+            var scoresArray = friend.scores;
+            // Iterate through each index (of each friend object) of the survey results' scoresArray - assign parameters score and index as placeholders for each score and each index
+            scoresArray.forEach(function (score, index) {
+                // For each index of the newFriend and existing friend's 'scores' array, reassign the sums variable to equal itself plus the difference between newFriends score value and existing friend's score value at the current index
+                differenceSum += Math.abs(newFriend.survey[index] - score); // Take the absolute value of each difference before adding them all together so there are no negative solutions
+            })
+            // Set a condition to check if the sums variable is less than the maximumDifference
+            if (differenceSum < maximumDifference) {
+                // If it is, reassign the value of maximumDifference to the differenceSum to compare to subsequent differences for each friend object
+                maximumDifference = differenceSum;
+                // Reassign friendMatch to be the friend object with the lower differenceSum - this will occur for each friend object until the friend object with the lowest difference is reached
+                friendMatch = friend;
+            }
+        });
+        // Push newFriend object to array of objects in api/friends.js
+        friendData.push(newFriend);
+        // Send the friendMatch object back as a JSON response
+        res.json(friendMatch);
+
         // For each friend object, isolate the array of survey results located at the key of 'scores:'
         // For each array of survey results, isolate the index and the value (score) at each index
         // For each value at each index of the survey 'scores', calculate the difference between it and the values at the newFriend's corresponding survey 'scores'
@@ -50,6 +73,7 @@ module.exports = function (app) {
             // Reassign friendMatch to equal the friend object with the current lowest difference during the conditional in Step 4
         // Once the closest match to newFriend has been found, display the result as a modal 'pop-up'
         // The modal should display both the name and picture of the closest match
+            // Adress this in 
         
     });
 };
--- server.js
+++ server.js
@@ -8,3 +8,17 @@ var app = express();
 // Or to use localhost:3000 in lieu of a dynamic port (For functionality on a local machine)
 var PORT = process.env.PORT || 3000;
 
+// Import apiRoutes and htmlRoutes which point our server to a series of "route" files
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Create an instance of Express to listen on the designated PORT (essentially, start the app)
+app.listen(PORT, function () {
+    // Log which PORT we're listening on
+    console.log("App listening on PORT: " + PORT);
+});
