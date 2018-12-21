3a79011ddf0d219be182bee29ba8766bd0810e2a
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-12-20 16:05:59 -0800

    clean up code comments in htmlRoutes; add modal to alert user and prevent them from submitting a survey if they didnt fill out their name and provide a photo URL

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -16,13 +16,4 @@ module.exports = function (app) {
         // (one directory up from current directory, then down into 'public')
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
-    // Create an instance of Express to use a callback function for any GET request made to a path besides '/survey'
-    // This functions as a default, catch-all route for unspecified paths
-    // https://stackoverflow.com/questions/22673218/default-route-in-express-js
-    // app.use(function (req, res) {
-        // When a GET request is made to an unspecified path, send the home.html file located in the 'app' > 'public' directory 
-        // (one directory up from current directory, then down into 'public')
-        // res.sendFile(path.join(__dirname + '../public/home.html'));
-    // });
-
 }
